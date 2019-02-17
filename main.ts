cf851a4f20e81016217391a9dff4dfeab27c7c9c
rsp <siripokarpirom@gmail.com>
2019-02-17 22:51:39 +0700

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,23 +1,22 @@
 
-/**
- * BH1750 Digital Ambient Light Sensor
- * Datasheet: https://www.mouser.com/ds/2/348/bh1750fvi-e-186247.pdf
- * Tested with GY-302 BH1750 Module
- */
-//% weight=100 color=#884411 icon="\uf185"
-
 enum BH1750Address {
+	//% block="0x23"
     ADDR_PIN_LOW  = 0x23,
+	//% block="0x5c"
     ADDR_PIN_HIGH = 0x5C
 }
+/*
+ * BH1750 functions
+ */
 
+//% color="#2c4e20" weight=100  
 namespace BH1750 {
 
     let i2c_addr = BH1750Address.ADDR_PIN_LOW
 
     /**
-     * set BH1750 Digital Ambient Light Sensor I2C address, 
-     * @param is the 7-bit I2C address of BH1750 module
+     * set the I2C address of BH1750, 
+     * @param addr is the 7-bit I2C address of BH1750 module
      */
     //% blockId="BH1750_SET_ADDRESS" block="bh1750 set address %addr"
     //% weight=100 blockGap=8
@@ -29,7 +28,7 @@ namespace BH1750 {
      * initialize BH1750 by turning the device on and
      * then sending a soft-reset command.
      */
-    //% blockId="BH1750_BEGIN" block="bh1750 begin"
+    //% blockId="BH1750_BEGIN" block="bh1750 begin %addr"
     //% weight=90 blockGap=8
     export function begin( addr : BH1750Address = BH1750Address.ADDR_PIN_LOW ): void {
         // power on
