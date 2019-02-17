c25f24ef4973b6eecdfd80c63fabe6be1c3577f1
rsp <siripokarpirom@gmail.com>
2019-02-17 22:43:32 +0700

    Add files via upload

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@
  * Datasheet: https://www.mouser.com/ds/2/348/bh1750fvi-e-186247.pdf
  * Tested with GY-302 BH1750 Module
  */
-//% weight=100 color=#004411 icon="\uf185"
+//% weight=100 color=#884411 icon="\uf185"
 
 enum BH1750Address {
     ADDR_PIN_LOW  = 0x23,
@@ -19,17 +19,17 @@ namespace BH1750 {
      * set BH1750 Digital Ambient Light Sensor I2C address, 
      * @param is the 7-bit I2C address of BH1750 module
      */
-    //% blockId="BH1750_SET_ADDRESS" block="set Address %addr"
+    //% blockId="BH1750_SET_ADDRESS" block="bh1750 set address %addr"
     //% weight=100 blockGap=8
     export function setAddress(addr: BH1750Address): void {
         i2c_addr = addr
     }
 
     /**
-     * initialize BH1750 by turning the devie on and
+     * initialize BH1750 by turning the device on and
      * then sending a soft-reset command.
      */
-    //% blockId="BH1750_BEGIN" block="begin"
+    //% blockId="BH1750_BEGIN" block="bh1750 begin"
     //% weight=90 blockGap=8
     export function begin( addr : BH1750Address = BH1750Address.ADDR_PIN_LOW ): void {
         // power on
@@ -43,7 +43,7 @@ namespace BH1750 {
     /**
      * turn on BH1750 to operate in continuous measurement mode
      */
-    //% blockId="BH1750_ON" block="turn on"
+    //% blockId="BH1750_ON" block="bh1750 turn on"
     //% weight=90 blockGap=8
     export function powerOn(): void {
         // power on
@@ -55,7 +55,7 @@ namespace BH1750 {
     /**
      * turn off BH1750, to reduce power consumption.
      */
-    //% blockId="BH1750_OFF" block="turn off"
+    //% blockId="BH1750_OFF" block="bh1750 turn off"
     //% weight=90 blockGap=8
     export function powerOff(): void {
         pins.i2cWriteNumber(i2c_addr, 0x00, NumberFormat.UInt8BE)
@@ -64,7 +64,7 @@ namespace BH1750 {
     /**
      * get the level of ambient light (lx)
      */
-    //% blockId="BH1750_GET_INTENSITY" block="get intensity (lx)"
+    //% blockId="BH1750_GET_INTENSITY" block="bh1750 get intensity (lx)"
     //% weight=80 blockGap=8
     export function getIntensity(): number {
         return Math.idiv(pins.i2cReadNumber(i2c_addr, NumberFormat.UInt16BE) * 5, 6)
